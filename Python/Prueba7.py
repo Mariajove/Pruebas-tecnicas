331106fae0d3f9d5590a559ed472953ea78e0af7
Mariajove <joremaria5@gmail.com>
2024-10-21 10:19:06 +0200

    Create Prueba7.py
    Ejercicio 7 hecho en MoureDev (hecho en Python).

---
+++ Python/Prueba7.py
@@ -0,0 +1,10 @@
+"""
+Ejericio 7:
+    Crea un programa que invierta el orden de una cadena de texto
+    sin usar funciones propias del lenguaje que lo hagan de forma automática.
+    - Si le pasamos "Hola mundo" nos retornaría "odnum aloH"
+"""
+
+#Simplemente es pedir que lea la cadena al revés
+cadena=input('Dime una frase')
+print(cadena[::-1])
