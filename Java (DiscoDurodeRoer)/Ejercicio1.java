d868b7973b1e25a8cbf1afa68eb5c4050cc8df40
Mariajove <joremaria5@gmail.com>
2024-10-23 19:55:12 +0200

    Create Ejercicio1.java

---
+++ Java (DiscoDurodeRoer)/Ejercicio1.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+public class Ejercicio1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+	    Pide un nombre por teclado y haz que te salude.
+
+	    Por ejemplo, si introduces "Fernando", la respuesta debe ser ¡Hola Fernando!
+
+	 */
+
+		String nombre=JOptionPane.showInputDialog("Dime tu nombre.");
+		System.out.println("¡Hola " + nombre +"!");
+
+		
+		
+		
+	}
+	
+	
+}
