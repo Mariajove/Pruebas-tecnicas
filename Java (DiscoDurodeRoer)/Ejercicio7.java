fbf96ac6d0b9bcf71ed8a5c47c589bdeea5dbd0e
Mariajove <joremaria5@gmail.com>
2024-10-23 20:07:16 +0200

    Update Ejercicio7.java

--- Java (DiscoDurodeRoer)/Ejercicio7.java
+++ Java (DiscoDurodeRoer)/Ejercicio7.java
@@ -1,9 +1,24 @@
+import java.util.*;
 
 public class Ejercicio7 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+    public static void main(String[] args) {
+    	/* 
+    	 * Mostrar la suma de los números hasta que el usuario introduzca un -1
+    	 */
+        Scanner escaner = new Scanner(System.in);
+        int numero;
+        int valor = 0;
 
-	}
+        System.out.println("Introduce el número que quieres que sume (introduce -1 para terminar):");
+        numero = escaner.nextInt();
 
+        while (numero != -1) {
+            valor += numero; 
+            System.out.println("Introduce el número que quieres que sume (introduce -1 para terminar):");
+            numero = escaner.nextInt();
+        }
+
+        System.out.println("El total de la suma de los números añadidos (sin contar -1) es de " + valor);
+    }
 }
